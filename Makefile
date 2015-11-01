a9901056d6f2734a20c5065978d483f58302163c
liscju <piotr.listkiewicz@gmail.com>
2015-11-01 21:09:32 +0100

    Add parsing conductor x position and conductor y position from command line

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	mpiexec -n 5 python main.py 1 10
+	mpiexec -n 5 python main.py 2 2 1 10
--- main.py
+++ main.py
@@ -21,30 +21,48 @@ import sys
 from mpi4py import MPI
 
 def usage():
-    print "You should invoke this with arguments: conductor_size, conductor_value"
+    print "You should invoke this with arguments: " \
+        "conductor_x_pos, conductor_y_pos, conductor_size, conductor_value"
     exit(-1)
-
-def get_conductor_size():
-    if len(sys.argv) == 1:
-        print "You have to pass conductor size as first argument"
+    
+def get_conductor_x_pos():
+    if len(sys.argv) < 2:
+        print "You have to pass conductor x pos as first argument"
         usage()
     else:
         return int(sys.argv[1])
-
-def get_conductor_value():
+    
+def get_conductor_y_pos():
     if len(sys.argv) < 3:
-        print "You have to pass conductor value as second argument"
+        print "You have to pass conductor x pos as second argument"
         usage()
     else:
         return int(sys.argv[2])
+
+def get_conductor_size():
+    if len(sys.argv) < 4:
+        print "You have to pass conductor size as third argument"
+        usage()
+    else:
+        return int(sys.argv[3])
+
+def get_conductor_value():
+    if len(sys.argv) < 5:
+        print "You have to pass conductor value as fitth argument"
+        usage()
+    else:
+        return int(sys.argv[4])
 
 def main():
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
+    conductor_x_pos = get_conductor_x_pos()
+    conductor_y_pos = get_conductor_y_pos()
     conductor_size = get_conductor_size()
     conductor_value = get_conductor_value()
-    print "Hello world from rank=", rank, " while size=", size, \
+    print "Hello world from rank=", rank, "pos=", (conductor_x_pos, conductor_y_pos), \
+        " while size=", size, \
         "and conductor_size=", conductor_size, " and conductor_value=", \
         conductor_value
 
