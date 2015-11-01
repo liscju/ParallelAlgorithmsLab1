f68fe6fa85e4ef4ad6078908b7730527810396df
liscju <piotr.listkiewicz@gmail.com>
2015-11-01 21:35:28 +0100

    Introduce GridHelper class which calculates if given point is border point or conductor point

---
+++ helpers.py
@@ -0,0 +1,16 @@
+__author__ = 'liscju'
+
+class GridHelper:
+    def __init__(self, size, conductor_x_pos, conductor_y_pos, conductor_size):
+        self.size = size
+        self.conductor_x_pos = conductor_x_pos
+        self.conductor_y_pos = conductor_y_pos
+        self.conductor_size = conductor_size
+
+    def is_border_point(self, x, y):
+        return y == 0 or y == self.size - 1 or \
+               x == 0 or x == self.size - 1
+
+    def is_conductor_point(self, x, y):
+        return self.conductor_x_pos <= x < self.conductor_x_pos + self.conductor_size and \
+            self.conductor_y_pos <= y < self.conductor_y_pos + self.conductor_size
