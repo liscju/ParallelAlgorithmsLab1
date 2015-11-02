76e127f01e3d1e2a5620cd45dfbf35d90a3f2c8d
liscju <piotr.listkiewicz@gmail.com>
2015-11-02 14:38:07 +0100

    Change test cases test_data.sh to more resonable numbers

--- test_data.sh
+++ test_data.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-SIZES_OF_ARRAY=("5" "10")
-CONDUCTOR_X=("50" "500")
-CONDUCTOR_Y=("50" "500")
-CONDUCTOR_SIZE=("10" "100")
-CONDUCTOR_VALUE=("20" "100")
-NUMBER_OF_ITERATION=("10" "1000")
+SIZES_OF_ARRAY=("5" "10" "15" "20" "25" "30" "35")
+CONDUCTOR_X=("2" "5" "7" "10" "12" "15" "17")
+CONDUCTOR_Y=("2" "5" "7" "10" "12" "15" "17")
+CONDUCTOR_SIZE=("1" "2" "3" "4" "5" "6" "7")
+CONDUCTOR_VALUE=("20" "50" "100" "200" "300" "400" "600")
+NUMBER_OF_ITERATION=("10" "100" "1000" "10000" "20000" "30000" "40000")
 
 for i in 0 1
 do
